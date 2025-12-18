267072885548193bec8c904f62d0d68a6915590b
bala-amypo <bala@amypo.in>
2025-12-18 10:06:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
@@ -25,4 +27,8 @@ public class StudentController {
         Student savedStudent = studentService.saveStudent(student);
         return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
+    @GetMapping("/getdata/{id}")
+    public Student getdata(@PathVariable Long id){
+         return service.get
+}
 }
--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -6,7 +6,7 @@
     import com.example.demo.entity.Student;
     import com.example.demo.repository.StudentRepository;
     import com.example.demo.service.StudentService;
-    import com.example.demo.exception
+    import com.example.demo.exception.ResourceNotFoundException;
     @Service
     public class StudentServiceImple implements StudentService {
         @Autowired
@@ -16,6 +16,6 @@
             return studentRepository.save(student);
         }
         public Student getStudentById(Long id){
-            return studentRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Student not found));
+            return studentRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Student not found"));
         }
-        }
+        
