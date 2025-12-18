b6ece9a6b033023a6c9af57aa44b5aa4798d06e8
bala-amypo <bala@amypo.in>
2025-12-18 08:16:21 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
-package com.example
+package com.example.demo.exception;
 
-@restControllerAdvice
-public calss GlobalExceptionHandler{
-    @ExceptionHandler(ResourceNotFoundException.class){
-        public ResponseEntity <String> handleNotFound(ResourceNotFoundException ex){
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
-        }
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
     }
 }
